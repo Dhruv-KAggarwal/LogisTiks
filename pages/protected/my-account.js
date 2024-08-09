a9167f3f9df0dd6ab52885b0161a6ad571bba2c2
Dhruv K. Aggarwal <165761845+Dhruv-KAggarwal@users.noreply.github.com>
2024-08-09 19:12:52 +0000

    new

--- pages/protected/my-account.js
+++ pages/protected/my-account.js
@@ -1,27 +1,28 @@
-// pages/protected/my-account.js
+// pages/my-account.js
 import Link from 'next/link';
-import withAuth from '../../components/withAuth';
+import Router from 'next/router';
 
-function MyAccount() {
+export default function MyAccount() {
   return (
     <div className="bg-[#fdfff0] min-h-screen flex flex-col md:flex-row">
+      {/* Column Container */}
       <div className="flex flex-col w-full md:w-1/4">
-        <Link href="/protected/my-trucks">
+        <Link href="/my-trucks">
           <div className="flex-1 p-4 text-center bg-blue-200 hover:bg-blue-300 transition-colors cursor-pointer">
             My Trucks
           </div>
         </Link>
-        <Link href="/protected/my-packages">
+        <Link href="/my-packages">
           <div className="flex-1 p-4 text-center bg-green-200 hover:bg-green-300 transition-colors cursor-pointer">
             My Packages
           </div>
         </Link>
-        <Link href="/protected/my-bids">
+        <Link href="/my-bids">
           <div className="flex-1 p-4 text-center bg-yellow-200 hover:bg-yellow-300 transition-colors cursor-pointer">
             My Bids
           </div>
         </Link>
-        <Link href="/protected/contact-us">
+        <Link href="/contact-us">
           <div className="flex-1 p-4 text-center bg-red-200 hover:bg-red-300 transition-colors cursor-pointer">
             Contact Us
           </div>
@@ -30,5 +31,3 @@ function MyAccount() {
     </div>
   );
 }
-
-export default withAuth(MyAccount);
